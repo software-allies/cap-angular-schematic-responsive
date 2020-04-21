82316f8b13125c7787bd234c4a7bf7527821996e
cesar_alonso <cesar_alonso_m_g@hotmail.com>
2020-04-21 09:45:45 -0500

    cap bootstrap to latest version run external schematic

--- src/cap-angular-schematic-responsive/index.ts
+++ src/cap-angular-schematic-responsive/index.ts
@@ -369,7 +369,7 @@ function addDeclarationToNgModule(options: ComponentOptions): Rule {
 }
 
 function addBootstrapSchematic() {
-    return externalSchematic('cap-angular-schematic-bootstrap', 'ng-add', { version: "4.0.0", skipWebpackPlugin: true });
+    return externalSchematic('cap-angular-schematic-bootstrap@latest', 'ng-add', { version: "4.0.0", skipWebpackPlugin: true });
 }
 
 function addHomeRoute(options: ComponentOptions): Rule {
