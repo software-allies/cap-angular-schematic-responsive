816434d7b62978f632d9edc63c8fb06dcc8a9f3d
Diego SanMo <diego14290764@itcg.edu.mx>
2021-03-17 10:33:36 -0600

    The collapse styles has been updated.

--- src/cap-angular-schematic-responsive/index.ts
+++ src/cap-angular-schematic-responsive/index.ts
@@ -338,19 +338,18 @@ function addCollapseFunctionality(): Rule {
     // Add footer to end of file
     const toAdd = 
 `setTimeout(() => {
-  const coll = document.getElementsByClassName("collapsible");
+  const coll = document.getElementsByClassName("collapsible__button");
   for (let i = 0; i < coll.length; i++) {
     coll[i].addEventListener("click", function () {
       this.classList.toggle("active");
       let content = this.nextElementSibling;
-      if (content.style.display === "block") {
+      if (content.style.display === "flex") {
         content.style.display = "none";
       } else {
-        content.style.display = "block";
+        content.style.display = "flex";
       }
     });
   }
-
 }, 1000);`;
       
     host.overwrite(filePath, `${toAdd}`);
