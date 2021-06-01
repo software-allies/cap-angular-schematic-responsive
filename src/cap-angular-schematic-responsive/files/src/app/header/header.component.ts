a52ed553fbb6c2c3ecfa894f4921f7ea02d49b6e
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2021-05-31 22:13:45 -0500

    CAP src/cap-angular-schematic-responsive/files/src/app/header/header.component.ts Remove unnecessary code.

--- src/cap-angular-schematic-responsive/files/src/app/header/header.component.ts
+++ src/cap-angular-schematic-responsive/files/src/app/header/header.component.ts
@@ -1,41 +1,32 @@
-import { Component, Inject, PLATFORM_ID, ChangeDetectionStrategy, OnInit } from '@angular/core';
-import { DOCUMENT } from '@angular/common';
-import { isPlatformBrowser } from '@angular/common';
+import { Component, ChangeDetectionStrategy, OnInit } from '@angular/core';
 import { ModalService } from '../modules/cap-responsive/components/modal/modal.service';
-import { LoadScriptService } from '../modules/cap-responsive/services/load-scripts.service';
 import { Subscription } from 'rxjs';
 import { Router, NavigationEnd, Event } from '@angular/router';
 <% if (auth && authService === 'auth0') { %>
 import { AuthenticationService } from 'cap-authentication';
-  import { StateService } from 'cap-authentication';
-  <% } %>
-    <% if (auth && authService === 'firebase') { %>
+import { StateService } from 'cap-authentication';
+<% } %>
+<% if (auth && authService === 'firebase') { %>
 import { AuthenticationService } from 'cap-authentication-firebase';
-      import { StateService } from 'cap-authentication-firebase';
-      <% } %>
-
-
+import { StateService } from 'cap-authentication-firebase';
+<% } %>
 @Component({
-        selector: 'app-header',
-        templateUrl: './header.component.html',
-        styleUrls: ['./header.component.scss'],
-        changeDetection: ChangeDetectionStrategy.Default
-      })
+  selector: 'app-header',
+  templateUrl: './header.component.html',
+  styleUrls: ['./header.component.scss'],
+  changeDetection: ChangeDetectionStrategy.Default
+})
 export class HeaderComponent implements OnInit {
 
   routerSubscription: Subscription;
   isTheHome = true;
-
-
+  
   constructor(
     <% if (auth) { %>
-  public authenticationService: AuthenticationService,
+    public authenticationService: AuthenticationService,
     public stateService: StateService,
-      <% } %>
-        private scriptService: LoadScriptService,
-      @Inject(PLATFORM_ID) private platformId: string,
-      @Inject(DOCUMENT) private document: Document,
-  public modalService: ModalService,
+    <% } %>
+    public modalService: ModalService,
     private router: Router
   ) {
     let scrolling = false;
@@ -54,75 +45,47 @@ export class HeaderComponent implements OnInit {
       }
     }, 100);
 
-  // Check if the route is the root for set the dark menu style
-  this.routerSubscription = router.events.subscribe((event: Event) => {
-    if (event instanceof NavigationEnd) {
-      this.isTheHome = (event.url === '/')
-    }
+    this.routerSubscription = this.router.events.subscribe((event: Event) => {
+      if (event instanceof NavigationEnd) {
+        if (event.url === '/' || event.url === '/home') {
+          const header = document.querySelector('#header') as HTMLElement;
+          header.style.height = "50vh";
+          const headerTitle = document.querySelector('#header__text-box') as HTMLElement;
+          headerTitle.style.display = "block";
+        } else {
+          const header = document.querySelector('#header') as HTMLElement;
+          header.style.height = "10vh";
+          const headerTitle = document.querySelector('#header__text-box') as HTMLElement;
+          headerTitle.style.display = "none";
+        }
+      }
   });
 }
 
-ngOnInit() {
-  this.addMenuScript();
-}
+  ngOnInit() { }
 
-ngOnDestroy() {
-  if (this.routerSubscription) {
-    this.routerSubscription.unsubscribe();
-  }
-}
+  openMenu() {
+    //Declaring the variables to get the HTML elements by ID
+    let navigationList = document.getElementById("list-menu") as HTMLElement;
+    let handlerMenu = document.getElementById("menu-handler") as HTMLElement;
+    let openIcon = document.getElementById("open") as HTMLElement;
+    let cancelIcon = document.getElementById("cancel") as HTMLElement;
 
-addMenuScript() {
-  if (isPlatformBrowser(this.platformId)) {
-    setTimeout(() => {
-      this.scriptService.load('webslidemenu').then(data => {
-      }).catch(error => console.log(error));
-    }, 0);
-  }
-}
+    console.log(navigationList.style.getPropertyValue('transition') === '');
 
-// Trick to close menu after options clicked on desktop and movile view
-clickLink() {
-  // Close menu in mobile view
-  const menu = this.document.getElementById('app');
-  if (menu) {
-    menu.classList.remove('wsactive');
-    // Close Megamenu
-    const element = this.document.querySelector('.wsmenu > .wsmenu-list > li:hover > .wsmegamenu');
-    if (element) {
-      element.classList.add('hidden');
-      // Clear style property to return the hover functionality after one second
-      setTimeout(function () {
-        element.classList.remove('hidden');
-      }, 1000);
+    navigationList.style.getPropertyValue('transition') === '' ? navigationList.style.transition = 'all 1s ease-in-out' : console.log('gg')
+    if (navigationList.style.left === '-30rem' || navigationList.style.getPropertyValue('left') === '') {
+      navigationList.style.transition = 'transition: all 1s ease-in-out';
+      openIcon.style.display = 'none';
+      cancelIcon.style.display = 'flex';
+      navigationList.style.left = '0rem';
+      handlerMenu.style.left = '21rem';
+    } else {
+      navigationList.style.left = '-30rem';
+      handlerMenu.style.left = '0rem';
+      cancelIcon.style.display = 'none';
+      openIcon.style.display = 'flex';
+      navigationList.style.transition = 'transition: all 1s ease-in-out';
     }
   }
-}
-
-openMenu() {
-  //Declaring the variables to get the HTML elements by ID
-  let navigationList = document.getElementById("list-menu") as HTMLElement;
-  let handlerMenu = document.getElementById("menu-handler") as HTMLElement;
-  let openIcon = document.getElementById("open") as HTMLElement;
-  let cancelIcon = document.getElementById("cancel") as HTMLElement;
-
-  console.log(navigationList.style.getPropertyValue('transition') === '');
-
-  navigationList.style.getPropertyValue('transition') === '' ? navigationList.style.transition = 'all 1s ease-in-out' : console.log('gg')
-  if (navigationList.style.left === '-30rem' || navigationList.style.getPropertyValue('left') === '') {
-    navigationList.style.transition = 'transition: all 1s ease-in-out';
-    openIcon.style.display = 'none';
-    cancelIcon.style.display = 'flex';
-    navigationList.style.left = '0rem';
-    handlerMenu.style.left = '21rem';
-  } else {
-    navigationList.style.left = '-30rem';
-    handlerMenu.style.left = '0rem';
-    cancelIcon.style.display = 'none';
-    openIcon.style.display = 'flex';
-    navigationList.style.transition = 'transition: all 1s ease-in-out';
-  }
-
-}
-
 }
