a2dacf921a87b01dd4d7299b8067cc92be34fa50
leninEmmanuel <lenin_emmanuel@softwareallies.com>
2021-05-31 22:08:52 -0500

    CAP src/cap-angular-schematic-responsive/files/src/assets/js/collapse.js Add a script using the .gitIgnore.

---
+++ src/cap-angular-schematic-responsive/files/src/assets/js/collapse.js
@@ -0,0 +1,14 @@
+setTimeout(() => {
+  const coll = document.getElementsByClassName("collapsible__button");
+  for (let i = 0; i < coll.length; i++) {
+    coll[i].addEventListener("click", function () {
+      this.classList.toggle("active");
+      let content = this.nextElementSibling;
+      if (content.style.display === "flex") {
+        content.style.display = "none";
+      } else {
+        content.style.display = "flex";
+      }
+    });
+  }
+}, 1000);
